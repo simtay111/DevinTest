28dfa7fa3a808c3111cd71b9b9a2ff28d4d4aa13
Devin <woohoo8987@yahoo.com>
2015-12-31 14:38:30 -0800

    Derp

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace DevinTest
+{
+
+    public class Person
+    {
+        public Person()
+        {
+
+
+        }
+
+
+    }
+    public class PersonTest
+    {
+        [SetUp]
+        public void setup()
+        {
+            Console.WriteLine("Starting Up");
+        }
+        [Test]
+        public void test()
+        {
+
+        }
+    }
+
+
+}
--- DevinTest/SetupExample.cs
+++ DevinTest/SetupExample.cs
@@ -1,12 +1,44 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace DevinTest
 {
     public class SetupExample
     {
+        public SetupExample()
+        {
+        
 
+
+        }
     }
 
     [TestFixture]
-    public class SetupExample
+    public class SetupExampleTests
+    {
+        private int MyNumber = 0;
+
+        [SetUp]
+        public void setup()
+        {
+            Console.WriteLine("In Setup lol");
+            MyNumber = 9;
+
+        }
+
+        [Test]
+        public void ShouldFireIfBaconIsInvolved()
+        {
+            MyNumber++;
+            Console.WriteLine("Within Test lol" + MyNumber);
+
+        }
+        [Test]
+        public void ShouldFireIfBaconIsInvolved2()
+        {
+            MyNumber++;
+            Console.WriteLine("Within Test2 lol" + MyNumber);
+
+        }
+    }
 }
