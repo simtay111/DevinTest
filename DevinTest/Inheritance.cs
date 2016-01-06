5696c8df1f224483bcb4b86e08ff2523719fd9a0
Simon Taylor <simtay111@gmail.com>
2016-01-06 09:57:50 -0800

    CLone

---
+++ DevinTest/Inheritance.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace DevinTest
+{
+    public class Animal
+    {
+        public string Color { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Zebra : Animal
+    {
+        public Zebra(string color, string name, int numberOfStripe)
+        {
+            Color = color;
+            Name = name;
+            NumberOfStripes = numberOfStripe;
+        }
+
+        public int NumberOfStripes { get; set; }
+    }
+
+    public class Lion : Animal
+    {
+        public int ZebrasEaten { get; set; }
+    }
+
+    public class Zoo
+    {
+        public void GetCOuntOfANimal()
+        {
+            var animals = new List<Animal>();
+
+            var zebra1 = new Zebra("Black", "Zeby", 44);
+
+
+            var zebra2 = new Zebra("Black", "Name", 55);
+            var lion = new Lion();
+
+            animals.Add(zebra1);
+            animals.Add(zebra2);
+            animals.Add(lion);
+
+            //animals length is 3
+            lion.Name = "Simon";
+
+            var integerList = new List<int> { 2, 3, 4 };
+            integerList.Add(23);
+            //integerList.Add("23");
+
+
+        }
+    }
+
+    public class GenericList<T>
+    {
+        public void Add(T item)
+        {
+
+        }
+    }
+
+
+}
---
+++ DevinTest/Scoping.cs
@@ -0,0 +1,31 @@
+﻿namespace DevinTest
+{
+    public class ScopingParent
+    {
+        public void MainMethod()
+        {
+            var secondary = new SecondaryClass();
+
+            //secondary.myPublicField
+
+            secondary.PublicMethod();
+
+        }
+
+    }
+
+    public class SecondaryClass
+    {
+        public string myPublicField;
+        private string myPrivateField;
+
+        public void PublicMethod()
+        {
+
+        }
+        private void PrivateMethod()
+        {
+
+        }
+    }
+}
