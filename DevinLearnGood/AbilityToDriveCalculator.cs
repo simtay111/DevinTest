437220f79cd1c0ba31f132478f1981285eddb405
Simon Taylor <simtay111@gmail.com>
2016-01-12 14:05:22 -0800

    Ability to drive calculator

---
+++ DevinLearnGood/AbilityToDriveCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace DevinLearnGood
+{
+    public class AbilityToDriveCalculator
+    {
+        public bool Calculate(int numberOfBeers, int numberOfHours, int numberOfCrackers)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DevinLearnGoodTest/AbilityToDriveCalculator.cs
@@ -0,0 +1,70 @@
+﻿using DevinLearnGood;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DevinLearnGoodTest
+{
+    [TestFixture]
+    public class AbilityToDriveCalculatorTests
+    {
+        private AbilityToDriveCalculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new AbilityToDriveCalculator();
+        }
+
+        [Test]
+        public void NotAbleToDriveAfterOneBeerPerHour()
+        {
+            var numberOfBeers = 3;
+            var numberOfHours = 3;
+            var numberOfCrackers = 0;
+
+            var isAbleToDrive = _calculator.Calculate(numberOfBeers, numberOfHours, numberOfCrackers);
+
+            isAbleToDrive.Should().BeTrue();
+
+            numberOfBeers = 4;
+            isAbleToDrive = _calculator.Calculate(numberOfBeers, numberOfHours, numberOfCrackers);
+
+            isAbleToDrive.Should().BeFalse();
+        }
+
+        [Test]
+        public void EveryTenCrackersAllowsForOneExtraBeer()
+        {
+            var numberOfBeers = 4;
+            var numberOfHours = 3;
+            var numberOfCrackers = 9;
+
+            var isAbleToDrive = _calculator.Calculate(numberOfBeers, numberOfHours, numberOfCrackers);
+
+            isAbleToDrive.Should().BeFalse();
+
+            numberOfCrackers = 10;
+            isAbleToDrive = _calculator.Calculate(numberOfBeers, numberOfHours, numberOfCrackers);
+
+            isAbleToDrive.Should().BeTrue();
+        }
+
+        [Test]
+        public void CrackersCanTakeAMaximumOf3BeersAway()
+        {
+            var numberOfBeers = 5;
+            var numberOfHours = 2;
+            var numberOfCrackers = 30;
+
+            var isAbleToDrive = _calculator.Calculate(numberOfBeers, numberOfHours, numberOfCrackers);
+
+            isAbleToDrive.Should().BeTrue();
+
+            numberOfCrackers = 40;
+            numberOfBeers = 6;
+            isAbleToDrive = _calculator.Calculate(numberOfBeers, numberOfHours, numberOfCrackers);
+
+            isAbleToDrive.Should().BeFalse();
+        }
+    }
+}
