bae118bb6a66ee0ec547fdb976b957b10ffcb4d1
Simon Taylor <simtay111@gmail.com>
2016-01-12 13:57:34 -0800

    Ingredients Tests

--- DevinLearnGood/IngredientPrinter.cs
+++ DevinLearnGood/IngredientPrinter.cs
@@ -1,7 +1,19 @@
-﻿namespace DevinLearnGood
+﻿using System.Collections.Generic;
+
+namespace DevinLearnGood
 {
     public class IngredientPrinter
     {
-         
+        public List<string> GetPrintable(List<Ingredient> ingredients)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string QuantityDescription { get; set; }
     }
 }
--- DevinLearnGoodTest/IngredientPrinterTests.cs
+++ DevinLearnGoodTest/IngredientPrinterTests.cs
@@ -1,4 +1,6 @@
-﻿using DevinLearnGood;
+﻿using System.Collections.Generic;
+using DevinLearnGood;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace DevinLearnGoodTest
@@ -13,7 +15,50 @@ namespace DevinLearnGoodTest
         {
             _printer = new IngredientPrinter();
         }
-        
-        //[Test]
+
+        [Test]
+        public void CanPrintOutIngredients()
+        {
+            var ingredients = new List<Ingredient>();
+            var ingredient1 = new Ingredient { Name = "Vodka", Quantity = 2, QuantityDescription = "Shots" };
+            var ingredient2 = new Ingredient { Name = "Mint", Quantity = 4, QuantityDescription = "Leaves" };
+            ingredients.Add(ingredient1);
+            ingredients.Add(ingredient2);
+
+            var results = _printer.GetPrintable(ingredients);
+
+            results[0].Should().Be("2 Shots of Vodka");
+            results[1].Should().Be("4 Leaves of Mint");
+        }
+
+        [Test]
+        public void CanPrintOutIngredientsWithNoQuantity()
+        {
+            var ingredients = new List<Ingredient>();
+            var ingredient1 = new Ingredient { Name = "Vodka", Quantity = 0, QuantityDescription = "Shots" };
+            var ingredient2 = new Ingredient { Name = "Mint", Quantity = 0, QuantityDescription = "Leaves" };
+            ingredients.Add(ingredient1);
+            ingredients.Add(ingredient2);
+
+            var results = _printer.GetPrintable(ingredients);
+
+            results[0].Should().Be("Vodka");
+            results[1].Should().Be("Mint");
+        }
+
+        [Test]
+        public void CanPrintOutIngredientsWithNoQuantityDescriptoins()
+        {
+            var ingredients = new List<Ingredient>();
+            var ingredient1 = new Ingredient { Name = "Vodka", Quantity = 2, QuantityDescription = "" };
+            var ingredient2 = new Ingredient { Name = "Mint", Quantity = 4, QuantityDescription = "" };
+            ingredients.Add(ingredient1);
+            ingredients.Add(ingredient2);
+
+            var results = _printer.GetPrintable(ingredients);
+
+            results[0].Should().Be("Vodka X 2");
+            results[1].Should().Be("Mint X 4");
+        }
     }
 }
