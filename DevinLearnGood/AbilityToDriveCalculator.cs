02acd8a77ebc150aeca4f37a2e053264c9fa7966
Devin Magee <devinmmagee@yahoo.com>
2016-01-18 13:48:13 -0800

    devins dail attempts

--- DevinLearnGood/AbilityToDriveCalculator.cs
+++ DevinLearnGood/AbilityToDriveCalculator.cs
@@ -5,9 +5,24 @@ namespace DevinLearnGood
 {
     public class AbilityToDriveCalculator
     {
+        private const int BeerToHourRatioLimit = 1;
+        private const int NumberOfCrackersToNegateABeer = 10;
+        private const int MaxNumberOfBeersCrackersCanReduce = 3;
+
         public bool CalculateIfAbleToDrive(double numberOfBeersDrankTotal, double numberOfHours, double numberOfCrackers)
         {
-            return true;
+            var numberOfBeersCrackersHelpedNegate = Math.Floor(numberOfCrackers / NumberOfCrackersToNegateABeer);
+
+            var isBeerRatioOverTheLimit = ((numberOfBeersDrankTotal - numberOfBeersCrackersHelpedNegate) / numberOfHours <= BeerToHourRatioLimit);
+
+            
+
+
+
+            return isBeerRatioOverTheLimit;
+
+
+
         }
     }
 }
