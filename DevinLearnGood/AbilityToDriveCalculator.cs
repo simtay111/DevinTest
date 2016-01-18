bbd031b9fbb218649367668d85989c8c7a244f77
Nate <nate.kirschner@gmail.com>
2016-01-18 10:28:59 -0800

    added to the ability to drive tests

--- DevinLearnGood/AbilityToDriveCalculator.cs
+++ DevinLearnGood/AbilityToDriveCalculator.cs
@@ -4,11 +4,26 @@
     {
         public bool CalculateIfAbleToDrive(double numberOfBeers, double numberOfHours, double numberOfCrackers)
         {
+            var crackersWillHelpWithBeers = (numberOfCrackers / 10);
+
+            var crackersHaveMaxedOutHelpingBloodAlcoholContent = crackersWillHelpWithBeers <= 3;
+
+            var crackersDidDeluteBloodAlcoholContent = crackersWillHelpWithBeers >= 1;
+
             var beersToHoursRatio = (numberOfBeers / numberOfHours);
 
             var isGreaterThanOnePerHour = beersToHoursRatio > 1;
 
             return !isGreaterThanOnePerHour;
+
+            //var beersToHoursRatio = (numberofbeers / numberofhours);
+
+            //var isGreaterThanOnePerHour = beersToHoursRatio > 1;
+
+            //return !isGreaterThanOnePerHour;
+
         }
+
+
     }
 }
