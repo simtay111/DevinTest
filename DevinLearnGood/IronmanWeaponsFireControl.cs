c42926ef0f4d38a73ea3acb3c3115a25c9eca138
Simon Taylor <simtay111@gmail.com>
2016-02-01 09:43:57 -0800

    Fire Control

---
+++ DevinLearnGood/IronmanWeaponsFireControl.cs
@@ -0,0 +1,26 @@
+﻿namespace DevinLearnGood
+{
+    public class IronmanWeaponsFireControl
+    {
+        public WeaponFireResult FireOnce(Weapon weapon)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class WeaponFireResult
+    {
+        public int DamageDone { get; set; }
+        public string Description { get; set; }
+        public Weapon WeaponUsed { get; set; }
+    }
+
+    public class Weapon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Ammunition { get; set; }
+        public int Damage { get; set; }
+        public int DamageMultiplierFromEnhancements { get; set; }
+    }
+}
---
+++ DevinLearnGoodTest/IronmanWeaponsFireControlTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using DevinLearnGood;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DevinLearnGoodTest
+{
+    [TestFixture]
+    public class IronmanWeaponsFireControlTests
+    {
+        private IronmanWeaponsFireControl _fireControl;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fireControl = new IronmanWeaponsFireControl();
+        }
+
+        [Test] //Simulation validation of the data
+        public void WeaponNeedsAnIdToFire()
+        {
+            var weapon = new Weapon { Id = 0, Ammunition = 44 };
+
+            var ex = Assert.Throws<Exception>(() => _fireControl.FireOnce(weapon));
+            ex.Message.Should().Be("Cannot fire weapon without an ID");
+        }
+
+        [Test] //Simulation validation of the data
+        public void WeaponCannotFireIfNoAmmunitionIsLeft()
+        {
+            var weapon = new Weapon { Id = 44, Ammunition = 0 };
+
+            var ex = Assert.Throws<Exception>(() => _fireControl.FireOnce(weapon));
+            ex.Message.Should().Be("Weapon (44) has no ammunition left");
+        }
+
+        [Test]//Simulating something readable despite no user input
+        public void ReturnsDescriptionWithoutNameBeingSet()
+        {
+            var weapon = new Weapon { Id = 44, Ammunition = 4 };
+
+            var result = _fireControl.FireOnce(weapon);
+
+            result.Description.Should().Be("Weapon (44): 'NO NAME' was fired and did 0 damage");
+        }
+
+        [Test] //Simulates how a full descriptoin can be provided
+        public void CreatesValidDescriptionWhenNameIsProvided()
+        {
+            var weapon = new Weapon { Id = 44, Damage = 3, Name = "Gun", Ammunition = 4 };
+
+            var result = _fireControl.FireOnce(weapon);
+
+            result.Description.Should().Be("Weapon (44): 'Gun' was fired and did 3 damage");
+        }
+
+        [Test]  //Simulating no user input
+        public void CanCalculateDamageWhenMultiplierIsNotSet()
+        {
+            var weapon = new Weapon { Id = 44, Damage = 3, Ammunition = 4 };
+
+            var result = _fireControl.FireOnce(weapon);
+
+            result.DamageDone.Should().Be(3);
+        }
+
+        [Test] //Simulates computation from instance values
+        public void MultipliesDamageByMultiplier()
+        {
+            var weapon = new Weapon { Id = 44, Damage = 3, DamageMultiplierFromEnhancements = 4, Ammunition = 4 };
+
+            var result = _fireControl.FireOnce(weapon);
+
+            result.DamageDone.Should().Be(12);
+        }
+
+        [Test]
+        public void ReturnsTheWeaponUsedToFireWithOneLessShotInTheAmmunition()
+        {
+            var weapon = new Weapon { Id = 44, Damage = 3, Ammunition = 4 };
+
+            var result = _fireControl.FireOnce(weapon);
+
+            result.WeaponUsed.Should().BeSameAs(weapon);
+            result.WeaponUsed.Ammunition.Should().Be(3);
+            weapon.Ammunition.Should().Be(3);
+        }
+    }
+}
