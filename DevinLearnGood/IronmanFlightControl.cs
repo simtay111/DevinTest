e714b03f56a0858197adb8552bac447606d97a61
Devin Magee <woohoo8987@yahoo.com>
2016-02-03 12:07:55 -0800

    latest and greatest iron man suit changes
    herp derp

--- DevinLearnGood/IronmanFlightControl.cs
+++ DevinLearnGood/IronmanFlightControl.cs
@@ -1,29 +1,55 @@
-﻿using System.Dynamic;
+﻿using System;
+using System.Dynamic;
 
 namespace DevinLearnGood
 {
     public class IronmanFlightControl
     {
-        public JetpackFlightResult CanMake(JetPack jetPack, int startingAltitude, int startingFuelInGallons, int desiredDistanceInMiles)
+
+        public JetpackFlightResult CanMake(Jetpack jetpack, int startingAltitude, int startingFuelInPlasmaUnits, int desiredDistanceInMiles)
         {
-            return null;
+            if (jetpack.Id <= 0)
+                throw new Exception("*JARVIS* \"Sir, I cannot allow you to fly to your destination without a valid flight authorization ID.\"");
+            if (jetpack.MaxAltitudeInThousandsOfFeet > 50000)
+            {
+                var failResult = new JetpackFlightResult();
+                failResult.CanMakeIt = false;
+                failResult.Message = "*JARVIS* \"Sir, I do not recommend flying to that altitude unless you want to gamble loss of flight capabilities due to high altitude freezing.\"";
+                return failResult;
+            }
+            if (jetpack.FuelEfficiencyInPercentage <= 0)
+            {
+                throw new Exception("*JARVIS* \"Sir, your total flight fuel consumption efficiency is 100% inadequte. Recommend shedding weight in the suits armaments.\"");
+            }
+            int addedWeight = jetpack.SuitWeightInPounds - 225;
+            jetpack.FuelEfficiencyInPercentage = (jetpack.FuelEfficiencyInPercentage - (addedWeight / 10));
+
+
+            var goodResult = new JetpackFlightResult();
+            goodResult.CanMakeIt = true;
+            goodResult.Message = $"*JARVIS* \"Sir, I've made the necessary preflight weight optimization calculations, and am informing you that your flight efficiency will be reduced down to {jetpack.FuelEfficiencyInPercentage}%.\"";
+            return goodResult;
         }
+
+
     }
 
     public class JetpackFlightResult
     {
         public int FuelBurned { get; set; }
         public bool CanMakeIt { get; set; }
+        public string Message { get; set; }
     }
 
-    public class JetPack
+    public class Jetpack
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int MaxAltitudeInThousandsOfFeet { get; set; }
         public int MaxFuel { get; set; }
         public int DirectionControlRating { get; set; }
-        public int Weight { get; set; }
-        public int FuelEffeciency { get; set; }
+        public int SuitWeightInPounds { get; set; }
+        public int TonyStarksWeightInPounds { get; set; }
+        public int FuelEfficiencyInPercentage { get; set; }
     }
 }
--- DevinLearnGoodTest/IronmanFlightControlTests.cs
+++ DevinLearnGoodTest/IronmanFlightControlTests.cs
@@ -1,14 +1,72 @@
-﻿namespace DevinLearnGoodTest
+﻿using System;
+using DevinLearnGood;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DevinLearnGoodTest
 {
+    [TestFixture]
     public class IronmanFlightControlTests
     {
-        //Ensure ID Is provided
-        //Return failed result if above max altitude
-        // Every ten pounds reduces fueld effeciency by 1.0     === verification should be on fuel burned
-        // Every Point down from Direction Control Rating (1- 10) costs 1 fuel effeciency === verification should be on fueld burned
-        // Returns failed result if not enough fuel to make it
+        private IronmanFlightControl _flightControl;
 
-        //ONCE DONE
-        //  Throws exception if starting fuel is over MAX fuel
+        [SetUp]
+        public void Setup()
+        {
+            _flightControl = new IronmanFlightControl();
+        }
+
+        [Test]
+        public void EnsureValidFlightAuthorizationIdIsProvided()
+        {
+            var jetpack = new Jetpack { Id = 0, Name = "Variable Specific Impulse Magnetoplasma Rocket (VASIMR)" };
+            var ex = Assert.Throws<Exception>(() => _flightControl.CanMake(jetpack, 0, 100, 42));
+            ex.Message.Should().Be("*JARVIS* \"Sir, I cannot allow you to fly to your destination without a valid flight authorization ID.\"");
+            jetpack.Id.Should().Be(0);
+        }
+
+        [Test]
+        public void ReturnsPrecalculatedFailResultForAltitudeTooHighForSuit()
+        {
+            var jetpack = new Jetpack { Id = 9, MaxAltitudeInThousandsOfFeet = 50001 };
+            var result = _flightControl.CanMake(jetpack, 0, 100, 42);
+            result.Message.Should().
+                Be("*JARVIS* \"Sir, I do not recommend flying to that altitude unless you want to gamble loss of flight capabilities due to high altitude freezing.\"");
+            jetpack.MaxAltitudeInThousandsOfFeet.Should().Be(50001);
+        }
+
+        [Test]
+        public void EveryTenPoundsOfTotalSuitWeightReducesFuelEfficiencyByFactorOfOnePercent()
+        {
+            var jetpack = new Jetpack { Id = 9, MaxAltitudeInThousandsOfFeet = 42000, SuitWeightInPounds = 240, TonyStarksWeightInPounds = 200, FuelEfficiencyInPercentage = 100 };
+            var result = _flightControl.CanMake(jetpack, 0, 100, 42);
+            result.Message.Should()
+                .Be($"*JARVIS* \"Sir, I've made the necessary preflight weight optimization calculations, and am informing you that your flight efficiency will be reduced down to {jetpack.FuelEfficiencyInPercentage}%.\"");
+        }
+
+        [Test]
+        public void EveryPointOfInefficientDirectionControlEffectsFuelEfficiencyByFactorOfOnePercent()
+        {
+            var jetpack = new Jetpack { Id = 9, MaxAltitudeInThousandsOfFeet = 42000, SuitWeightInPounds = 240, TonyStarksWeightInPounds = 200, FuelEfficiencyInPercentage = 100, DirectionControlRating = 100 };
+            var result = _flightControl.CanMake(jetpack, 0, 100, 42);
+            result.Message.Should()
+                .Be($"*JARVIS* \"Sir, I've made the necessary preflight direction control optimization calculations, and am informing you that your flight efficiency will be reduced down to {jetpack.FuelEfficiencyInPercentage}%.\"");
+        }
+
+        [Test]
+        public void ReturnsPrecalculatedFailResultForNotEnoughTotalFlightFuel()
+        {
+            // nerp
+        }
+
     }
 }
+
+//Ensure ID Is provided
+//Return failed result if above max altitude
+// Every ten pounds reduces fueld effeciency by 1.0     === verification should be on fuel burned
+// Every Point down from Direction Control Rating (1- 10) costs 1 fuel effeciency === verification should be on fueld burned
+// Returns failed result if not enough fuel to make it
+
+//ONCE DONE
+//  Throws exception if starting fuel is over MAX fuel
