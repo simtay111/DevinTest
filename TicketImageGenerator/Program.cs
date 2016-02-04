9c8c9ea3d803773fe2d993602f465a4a7775337a
Devin Magee <woohoo8987@yahoo.com>
2016-02-03 16:57:34 -0800

    ticket details format
    moo

--- TicketImageGenerator/Program.cs
+++ TicketImageGenerator/Program.cs
@@ -6,6 +6,7 @@ using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Net.Cache;
 using Gma.QrCodeNet.Encoding;
 
 namespace TicketImageGenerator
@@ -14,24 +15,50 @@ namespace TicketImageGenerator
     {
         static void Main(string[] args)
         {
-            var image = new Bitmap(1000, 1000);
-            var drawing = Graphics.FromImage(image);
+          var gen = new TicketImageGenerator();
 
-            Font font = new Font(FontFamily.GenericMonospace, 24);
+            var request =  new TicketImageGeneratorRequestDetails();
+            request.ClientName = "JOHN JACOB JINGLE HEIMER SCHMIDT";
+
+            gen.PrintImage(request);
+            
+        }
+    }
+
+    public class TicketImageGeneratorRequestDetails
+    {
+        public string ClientName { get; set; }
+        public int ClientAge { get; set; }
+        public string TicketType { get; set; }
+        public string PricingType { get; set; }
+        public DateTime ClientEndTime { get; set; }
+    }
+
+    public class TicketImageGenerator
+    {
+        public void PrintImage(TicketImageGeneratorRequestDetails request)
+        {
+            var image = new Bitmap(900, 500);
+            var drawing = Graphics.FromImage(image);
+            
+            Font font = new Font(FontFamily.GenericSerif, 18);
+            Font otherFont = new Font(FontFamily.GenericSerif, 28);
             Brush textBrush = new SolidBrush(Color.Black);
             Brush backgroundBrush = new SolidBrush(Color.White);
 
-            drawing.FillRectangle(backgroundBrush, 0, 0, 1000, 1000);
+            drawing.FillRectangle(backgroundBrush, 0, 0, 900, 500);
 
-            drawing.DrawString("Simon Taylor", font, textBrush, 0, 0);
-            drawing.DrawString("Start Date: " + DateTime.Now.ToShortDateString(), font, textBrush, 0, 50);
-            drawing.DrawString("Start Time: " + DateTime.Now.ToShortTimeString(), font, textBrush, 0, 100);
-            drawing.DrawString("End Time: " + DateTime.Now.ToShortTimeString(), font, textBrush, 0, 150);
+            drawing.DrawString($"Name: {request.ClientName}", font, textBrush, 0, 30);
+            drawing.DrawString($"Age: {request.ClientAge}", font, textBrush, 0, 130);
+            drawing.DrawString($"Ticket Type: {request.TicketType}", font, textBrush, 0, 230);
+            drawing.DrawString($"END TIME: {request.ClientEndTime}" + DateTime.Now.ToShortTimeString(), font, textBrush, 0, 330);
+            drawing.DrawString($"Pricing Type: {request.PricingType}", otherFont, textBrush, 0, 430);
+            
             var encoder = new QrEncoder();
             var thingy = encoder.Encode(Guid.NewGuid().ToString());
 
-            var renderer = new Gma.QrCodeNet.Encoding.Windows.Controls.Renderer(15, textBrush, backgroundBrush);
-            renderer.Draw(drawing, thingy.Matrix, new Point(200, 50));
+            var renderer = new Gma.QrCodeNet.Encoding.Windows.Controls.Renderer(11, textBrush, backgroundBrush);
+            renderer.Draw(drawing, thingy.Matrix, new Point(525, 80));
 
             drawing.Save();
             textBrush.Dispose();
@@ -40,8 +67,6 @@ namespace TicketImageGenerator
             image.Save(@"C:\sampleimage.bmp");
 
             Process.Start(@"C:\sampleimage.bmp");
-
-
         }
     }
 }
