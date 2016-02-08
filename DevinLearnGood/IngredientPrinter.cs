08fbfca6e8e8aac4780725e6a10e2ec4cafe28ec
Devin Magee <woohoo8987@yahoo.com>
2016-02-08 15:25:14 -0800

    Attempting the Ingredient Printer >.<
    moo

--- DevinLearnGood/IngredientPrinter.cs
+++ DevinLearnGood/IngredientPrinter.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace DevinLearnGood
 {
@@ -6,6 +8,20 @@ namespace DevinLearnGood
     {
         public List<string> GetPrintable(List<Ingredient> ingredients)
         {
+            // I need to write code that will make a total string from the 3 properties of the Ingrediant List
+            // as well as add some filler string text between those properties.
+
+            // GetPrintable(List<Ingredient> ingredients) = new List<Ingredient>();
+
+            Console.WriteLine($"2 Shots of Vodka");
+            Console.WriteLine($"4 Leaves of Mint");
+
+            foreach (var totalDrink in ingredients)
+            {
+                totalDrink.
+            }
+
+
             throw new System.NotImplementedException();
         }
     }
